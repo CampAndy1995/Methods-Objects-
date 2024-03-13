e8b6454007d9fe2f64edb664a889d89b0fa520db
Andrew Campbell <144943158+CampAndy1995@users.noreply.github.com>
2024-03-13 13:35:41 -0700

    Add files via upload
    Perform these actions and create a console app that includes the following:
    
    
    Create a class called Person and give it two properties, each of data type string. One called FirstName, the other LastName.
    
    Give this class a void method called SayName() that takes no parameters and simply writes the person's full name to the console in the format of: “Name: [full name]”.
    
    Create another class called Employee and have it inherit from the Person class. Give the Employee class a property called Id and have it be of data type int.
    
    Inside of the Main method, instantiate and initialize an Employee object with a first name of “Sample” and a last name of “Student”.
    
    Call the superclass method SayName() on the Employee object.
    
    Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+// Define a class called Person with two properties: FirstName and LastName
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    // Define a method called SayName that writes the person's full name to the console
+    public void SayName()
+    {
+        Console.WriteLine("Name: " + FirstName + " " + LastName);
+        Console.ReadLine();
+    }
+}
+
+// Define a class called Employee that inherits from the Person class
+public class Employee : Person
+{
+    // Add a property called Id of type int
+    public int Id { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Instantiate and initialize an Employee object with a first name of "Sample" and a last name of "Student"
+        Employee employee = new Employee
+        {
+            FirstName = "Andrew",
+            LastName = "Campbell"
+        };
+
+        // Call the superclass method SayName() on the Employee object
+        employee.SayName();
+
+        // Keep the console window open
+        Console.ReadLine();
+    }
+}
+
